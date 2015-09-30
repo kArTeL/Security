7c6dd05c0340c184817be4f8dac942e77aa412ea
Neil García Vargas <Neil@Neils-MacBook-Pro.local>
2015-09-30 00:02:57 -0600

    route module created, this make possible a abstraction beetween the request and the app,js still looking for a mysql connector :(

--- FruitStore/Api/app.js
+++ FruitStore/Api/app.js
@@ -1,5 +1,11 @@
+/** Nodejs framework modules necessary to response the request
+*/
 var http = require('http');
-var tokenGenerator = require('./Tokengenerator.js');
+
+
+var route = require('./Route.js')
+
+
 
 function parseCookies (request) {
     var list = {},
@@ -13,21 +19,27 @@ function parseCookies (request) {
     return list;
 }
 
+///Create a nodejs server
 http.createServer(function (request, response) {
 
+  route.route(request,response);
+
+
   // To Read a Cookie
-  var cookies = parseCookies(request);
+//  var cookies = parseCookies(request);
 
   // To Write a Cookie
-  response.writeHead(200, {
-    'Set-Cookie': 'mycookie=test',
-    'Content-Type': 'text/plain'
-  });
-  //console.log(tokenGenerator)
-  var token = tokenGenerator.generateToken();
-  console.log(token)
-  response.end(token);
+  // response.writeHead(200, {
+  //   'Set-Cookie': 'mycookie=test',
+  //   'Content-Type': 'text/plain'
+  // });
+  //Toke generator
+  // var token = tokenGenerator.generateToken();
+  // console.log(token)
+
+  // Redirect
+
 
 }).listen(8080);
 
-console.log('Server running at http://127.0.0.1:8080/');
+//console.log('Server running at http://127.0.0.1:8080/');
